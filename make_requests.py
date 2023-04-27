8940f4a5518921c5bc813c6f09f2792fffc25575
BuildTools <unconfigured@null.spigotmc.org>
2023-04-27 22:16:25 +0300

    Make get_all_items func

---
+++ make_requests.py
@@ -0,0 +1,16 @@
+import requests
+from add_queries import open_connection
+
+
+def get_all_items():
+    connection = open_connection()
+    cursor = connection.cursor()
+    query = """SELECT * FROM buff_tracker"""
+    cursor.execute(query)
+    results = cursor.fetchall()
+    connection.close()
+    return results
+
+
+def exec_requests(items):
+    return
