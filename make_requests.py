507999cdeeb261ebb2a2fc46a7ff7c8fc56cf172
BuildTools <unconfigured@null.spigotmc.org>
2023-04-28 18:57:07 +0300

    Make request send function and process price data

--- make_requests.py
+++ make_requests.py
@@ -1,8 +1,12 @@
-import requests
 from add_queries import open_connection
+import urllib.request
+import json
 
 
-def get_all_items():
+URL = 'https://buff.163.com/api/market/goods/sell_order?game=csgo'
+
+
+def get_all_items_from_db():
     connection = open_connection()
     cursor = connection.cursor()
     query = """SELECT * FROM buff_tracker"""
@@ -12,8 +16,26 @@ def get_all_items():
     return results
 
 
-def exec_requests(items):
-    return
+def send_request(item_goods_id):
+    username = 'buffbot'
+    password = 'bUffbot123123987987'
+    entry = ('http://customer-%s:%s@pr.oxylabs.io:7777' %
+             (username, password))
+    query = urllib.request.ProxyHandler({
+        'http': entry,
+        'https': entry,
+    })
+    execute = urllib.request.build_opener(query)
+    return json.loads(execute.open(f"{URL}&goods_id={item_goods_id}").read())["data"]["items"]
 
 
-print(get_all_items())
+def process_data(item_json):
+    price_list = list()
+    for item_data in item_json:
+        price_list.append(item_data["price"])
+
+    return price_list
+
+
+for item in get_all_items_from_db():
+    print(process_data(send_request(item[2])))
