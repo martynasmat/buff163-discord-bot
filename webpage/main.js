64564fdb8b6d47fba62c00bd5d16c8268d9ffaf1
mykolassl <emykolas@gmail.com>
2023-05-02 21:55:05 +0300

    Item adding logic

--- webpage/main.js
+++ webpage/main.js
@@ -7,11 +7,35 @@ const IDS = {
 	rokazz: 598398078268997632,
 };
 
+const IDlist = new Set();
+
+const form = document.querySelector('#form');
 const btnAddItem = document.querySelector('#btn-add');
 const btnSubmitItems = document.querySelector('#btn-submit');
-const selectedID = document.querySelector('#discord-id');
+const goodsID = document.querySelector('#item-id');
+const selectUser = document.querySelector('#discord-id');
+const goodsIDContainer = document.querySelector('#item-id-container');
 
-selectedID.addEventListener('change', (e) => {
-	const user = e.target.value;
-	console.log(IDS[user]);
+form.addEventListener('submit', (e) => e.preventDefault());
+
+btnSubmitItems.addEventListener('click', (e) => {
+	const selectedUser = selectUser.value;
+	console.log(selectedUser);
+	console.log(IDS[selectedUser]);
+});
+
+btnAddItem.addEventListener('click', () => {
+	if (!goodsID.value || IDlist.has(goodsID.value))
+		return alert('Prasišviesk galvą');
+
+	const id = goodsID.value;
+
+	IDlist.add(id);
+	const div = document.createElement('div');
+	div.innerText = `${id}`;
+	goodsIDContainer.appendChild(div);
+
+	console.log(IDlist);
+
+	goodsID.value = '';
 });
