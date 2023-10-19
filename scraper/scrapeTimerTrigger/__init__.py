533dd95859b83dc998b2208ab6b291f7a0d08d71
martynasmat <martynasmat03@gmail.com>
2023-10-19 22:57:45 +0300

    Change env variable names

--- scraper/scrapeTimerTrigger/__init__.py
+++ scraper/scrapeTimerTrigger/__init__.py
@@ -20,8 +20,8 @@ def main(mytimer: func.TimerRequest) -> None:
 
     connection = mysql.connector.connect(
         host=os.getenv('DB_HOST'),
-        user=os.getenv('DB_USERNAME2'),
-        password=os.getenv('DB_PASSWORD2'),
+        user=os.getenv('DB_USERNAME'),
+        password=os.getenv('DB_PASSWORD'),
         database=os.getenv('DB_NAME')
     )
 
